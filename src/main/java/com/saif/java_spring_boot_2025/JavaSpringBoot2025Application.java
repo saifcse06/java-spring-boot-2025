a20a012b5344a80ba2a6d370d80264f0790d5336
Saif Uddin <saifcse06@gmail.com>
2025-05-05 22:25:33 +0600

    Initial commit

---
+++ src/main/java/com/saif/java_spring_boot_2025/JavaSpringBoot2025Application.java
@@ -0,0 +1,13 @@
+package com.saif.java_spring_boot_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBoot2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBoot2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/saif/java_spring_boot_2025/JavaSpringBoot2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saif.java_spring_boot_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBoot2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
